6851584c3cec90d2523a1cefd6fef7c452cb9ca3
jrakitzis <75811733+jrakitzis@users.noreply.github.com>
2021-04-27 17:59:55 -0400

    number cubed task from week 4

---
+++ Week4Tasks/NumberCubed/NumberCubed/NumberCubed.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+double cube(double num);
+
+
+int main()
+{
+   
+    int num;
+    double c;
+
+    //Inputting number from user
+    cout << "Enter any number: " << endl;
+    cin >> num;
+    
+    // call function
+    c = cube(num);
+
+    cout << "Cube of " << num << " is " << c;
+
+    return 0;
+}
+
+//Function to find cube of any number
+double cube(double num)
+{
+    return (num * num * num);
+}
