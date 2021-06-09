24725c9b1a1b55b0d84f6ce8d3096e0e261a4d8d
jrakitzis <75811733+jrakitzis@users.noreply.github.com>
2021-06-09 15:04:29 -0400

    ascii game

--- ASCII Game/ASCII Game/ASCII Game/Main.cpp
+++ ASCII Game/ASCII Game/ASCII Game/Main.cpp
@@ -1,9 +1,11 @@
 // Includes
 #include <Windows.h>
+#include<chrono>
 
 //Defines
 #define WIDTH 150
 #define HEIGHT 40
+#define FRAME_RATE 30
 
 // console parameters
 /////////////////////
@@ -28,13 +30,29 @@ HANDLE wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
 HANDLE rHnd = GetStdHandle(STD_INPUT_HANDLE);
 #pragma endregion
 
-//Game Parameters
-/////////////////////
-const unsigned char characterToPrint = 176;
+//TypeDefs
+typedef std::chrono::steady_clock::time_point Time;
+typedef std::chrono::high_resolution_clock HiResClock;
+typedef std::chrono::duration<float> TimeDiff;
+
+
 
 // Game Constants
-const int playerCharacter = '@';
-const int playerColor = 7;
+
+const int PlayerWidth = 2;
+const int PlayerHeight = 3;
+const int PlayerCharacters[PlayerWidth * PlayerHeight] =
+{
+	'_','_',
+	'|','|',
+	'/','\\'
+};
+const int PlayerColors[PlayerWidth * PlayerHeight] =
+{
+	0xF,0xF,
+	0xF,0xF,
+	0xE,0xE,
+};
 
 const int KeyEsc = VK_ESCAPE;
 const int KeyW = 'W';
@@ -44,7 +62,14 @@ const int KeyD = 'D';
 
 //Game Variables
 int playerXPos = 0;
-int PlayerYPos = 0;
+int playerYPos = 0;
+bool exitGame = false;
+float deltaTime = 0.0f;
+int frameCounter = 0;
+Time previousFrameTime = HiResClock::now();
+
+//functions 
+int ClampInt(int intToClamp, int lowerLimit, int upperLimit);
 
 int main()
 {
@@ -52,39 +77,99 @@ int main()
 	//////////////////////////////
 	// set the title
 	SetConsoleTitle(L"Title of my console");
-	
+
 	// set the screens buffer size
 	SetConsoleScreenBufferSize(wHnd, bufferSize);
 
 	//set the window size
 	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
-	//////////////////////////////////////////////
 
-	// Exit game flag
-	bool exitGame = false;
+
+
+
 
 	//Game Loop
 	while (!exitGame)
 	{
-		//Checking for input 
-		if (GetAsyncKeyState(VK_ESCAPE))
-		{
-			//set our exit game flag
-			exitGame = true;
-		}
+		//calculate our delta time (time since last frame)
+		Time currentFrameTime = HiResClock::now();
+		TimeDiff diff = currentFrameTime - previousFrameTime;
+		deltaTime = diff.count();
+		frameCounter++;
 
-		//Nested loop to set each character to a random color 
-		for (int y = 0; y < HEIGHT; ++y)
+		if (deltaTime >= (1.0f / FRAME_RATE))
 		{
-			for (int x = 0; x < WIDTH; ++x)
+			// cashe the timestamp of this frame
+			previousFrameTime = currentFrameTime;
+
+			//Checking for input 
+			if (GetAsyncKeyState(KeyEsc))
 			{
-				consoleBuffer[x + WIDTH * y].Char.AsciiChar = characterToPrint;
-				consoleBuffer[x + WIDTH * y].Attributes = rand() % 128;
+				//set our exit game flag
+				exitGame = true;
 			}
+			if (GetAsyncKeyState(KeyW))
+			{
+				--playerYPos;
+			}
+			if (GetAsyncKeyState(KeyA))
+			{
+				--playerXPos;
+			}
+			if (GetAsyncKeyState(KeyS))
+			{
+				++playerYPos;
+			}
+			if (GetAsyncKeyState(KeyD))
+			{
+				++playerXPos;
+			}
+			//clamp the input
+			playerXPos = ClampInt(playerXPos, 0, (WIDTH - 1));
+			playerYPos = ClampInt(playerYPos, 0, (HEIGHT - 1));
+
+
+			//clear the previous frame before we start the build for the next frame
+			for (int i = 0; i < (WIDTH * HEIGHT); ++i)
+			{
+				consoleBuffer[i].Char.AsciiChar = 0;
+				consoleBuffer[i].Attributes = 0;
+			}
+
+
+
+
+			// Draw Player Image
+			for (int y = 0; y < PlayerHeight; ++y)
+			{
+				for (int x = 0; x < PlayerWidth; ++x)
+				{
+					consoleBuffer[(playerXPos + x) + WIDTH * (playerYPos + y)].Char.AsciiChar = PlayerCharacters[x + PlayerWidth * y];
+					consoleBuffer[(playerXPos + x) + WIDTH * (playerYPos + y)].Attributes = PlayerColors[x + PlayerWidth * y];
+				}
+
+			}
+			// kick the draw
+			WriteConsoleOutputA(wHnd, consoleBuffer, characterBufferSize, characterPosition, &consoleWriteArea);
+
+
+
 		}
-
-		WriteConsoleOutputA(wHnd, consoleBuffer, characterBufferSize, characterPosition, &consoleWriteArea);
-
+		return 0;
 	}
-
+}
+int ClampInt(int intToClamp, int lowerLimit, int upperLimit)
+{
+	if (intToClamp <= lowerLimit)
+	{
+		return lowerLimit;
+	}
+	else if (intToClamp >= upperLimit)
+	{
+		return upperLimit;
+	}
+	else
+	{
+		return intToClamp;
+	}
 }
